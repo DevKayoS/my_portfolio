1cf618aca1da192b5b4322dd5e4bb971252e417b
kayo vinicius <kayoribeirodev@gmail.com>
2024-02-26 22:17:30 -0300

    atualizando

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,10 @@
-import logo from '../assets/perfil.png'
+import perfil from '../assets/perfil.png'
 
 export function Header(){
   return(
     <div className="w-full bg-zinc-800 h-16  flex fixed m-0">
       <div className='w-72 ml-2 flex-row gap-2 flex justify-center'>
-        <img src={logo} className="rounded-md mt-3  size-10" />
+        <img src={perfil} className="rounded-md mt-3  size-10" />
         <div className='flex items-center justify-center'>
           <p className='text-sm hover:text-zinc-300 hover:underline text-zinc-500'>kayoribeirodev@gmail.com</p>
         </div>
